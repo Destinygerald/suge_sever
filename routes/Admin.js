71b6bee9c664d50cd26c28cd8b0429566aa8efdd
Destinygerald <destinygerald85@gmail.com>
2025-03-04 17:47:27 +0100

    Fix: cors enabled

--- routes/Admin.js
+++ routes/Admin.js
@@ -4,10 +4,10 @@ const { authAdmin } = require('../middleware.js')
 
 const Route = express.Router()
 
-Route.all('/', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Contol-Allow-Headers', 'X-Requested-With')
-})
+// Route.all('/', function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*')
+//     res.header('Access-Contol-Allow-Headers', 'X-Requested-With')
+// })
 
 Route.post('/login', auth)
 
