e507692d7c1efc724b6bbd326b86a14636c7c5a0
Destinygerald <destinygerald85@gmail.com>
2025-03-05 15:44:33 +0100

    Fixed: Added the img body

--- controllers/Blog.js
+++ controllers/Blog.js
@@ -226,7 +226,7 @@ async function blog (req, res) {
 async function editBlog(req, res) {
     try {
         const { id } = req.params
-        const { title, content, readTime } = req.body
+        const { title, content, readTime, imag } = req.body
 
         if (!id) {
             return res.status(404).json({
@@ -247,7 +247,8 @@ async function editBlog(req, res) {
         const data = {
             title,
             content,
-            readTime
+            readTime,
+            img
         }
         
         await updateBlog(id, data)
