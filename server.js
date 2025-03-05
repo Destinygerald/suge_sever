104e76f855bc60a520d47036238223394e7e35d9
Destinygerald <destinygerald85@gmail.com>
2025-03-05 14:32:52 +0100

    Fixed: Increased the size of the request body

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const { errorHandler, asyncWrapper } = require('./middleware.js')
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(
     cors({
