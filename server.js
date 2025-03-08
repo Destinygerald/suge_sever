563ae5216486d394cd486ffe70b3eecbcbf3784e
Destinygerald <destinygerald85@gmail.com>
2025-03-08 09:33:32 +0100

    Fix: added suge.co.uk to the cors standrad

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(
     cors({
-        origin: ['http://localhost:5173', 'https://suge-3a6v.onrender.com', 'www.suge.co.uk', 'www.sugeuk.com'],
+        origin: ['http://localhost:5173', 'https://suge-3a6v.onrender.com', 'www.suge.co.uk', 'www.sugeuk.com', 'http://suge.co.uk'],
         credentials: true,
         optionsSuccessStatus: 200 
     })
