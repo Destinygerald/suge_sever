8f533718fa3a793f87ee03b1d86df4d749485c5f
Destinygerald <destinygerald85@gmail.com>
2025-03-08 21:07:06 +0100

    Fix: removed res.cookies from the login

--- server.js
+++ server.js
@@ -8,30 +8,30 @@ const { errorHandler, asyncWrapper } = require('./middleware.js')
 
 const app = express();
 
-app.use(function(req, res, next) {
-    // res.header("Access-Control-Allow-Origin", "*");
-    const allowedOrigins = ['http://localhost:5173', 'https://www.suge.co.uk', 'http://www.suge.co.uk', 'https://suge-3a6v.onrender.com', 'http://suge.co.uk', 'http://sugeuk.com', 'https://sugeuk.com'];
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-         res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-credentials", true);
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-    next();
-});
+// app.use(function(req, res, next) {
+//     // res.header("Access-Control-Allow-Origin", "*");
+//     const allowedOrigins = ['http://localhost:5173', 'https://www.suge.co.uk', 'http://www.suge.co.uk', 'https://suge-3a6v.onrender.com', 'http://suge.co.uk', 'http://sugeuk.com', 'https://sugeuk.com'];
+//     const origin = req.headers.origin;
+//     if (allowedOrigins.includes(origin)) {
+//          res.setHeader('Access-Control-Allow-Origin', origin);
+//     }
+//     // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//     res.header("Access-Control-Allow-credentials", true);
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+//     next();
+// });
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
-// app.use(
-//     cors({
-//         origin: ['http://localhost:5173', 'https://suge-3a6v.onrender.com', 'http://suge.co.uk', 'http://sugeuk.com', 'https://suge.co.uk', 'https://sugeuk.com'],
-//         credentials: true,
-//         optionsSuccessStatus: 200 
-//     })
-// );
+app.use(
+    cors({
+        origin: ['http://localhost:5173', 'https://suge-3a6v.onrender.com', 'https://www.suge.co.uk', 'http://sugeuk.com', 'http://www.suge.co.uk', 'https://sugeuk.com'],
+        credentials: true,
+        optionsSuccessStatus: 200 
+    })
+);
 
 
 app.use('/admin', admin)
